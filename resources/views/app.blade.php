8318b9d762b0f04066a87924010f023edaf490ea
Jean Piero <jeanusat@gmail.com>
2026-03-24 17:36:45 -0500

    'sdf'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'CLIEGA') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css">
-        <!-- Styles / Scripts -->
-       {{--  @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    <title>{{ config('app.name', 'CLIEGA') }}</title>
+    <link rel="icon" href="https://sisega.gamarrafirma.com/img/marca_agua.svg" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css">
+    <!-- Styles / Scripts -->
+    {{-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
           
         @endif --}}
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body>
-        <div id="app"></div>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-    </body>
+<body>
+    <div id="app"></div>
+
+</body>
+
 </html>
